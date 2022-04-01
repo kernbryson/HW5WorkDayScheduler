ce61b7491ca4747d1730d5d33b87ef53b996a114
Bryson Kern <kernbryson@yahoo.com>
2022-04-01 19:50:25 -0400

    added local storage

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,6 +1,5 @@
 // save reference to important DOM elements
 var timeDisplayEl = $("#time-display");
-let times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 // handle displaying the time
 let save1 = $("#save1");
 let save2 = $("#save2");
@@ -16,77 +15,140 @@ function displayTime() {
   timeDisplayEl.text(rightNow);
 }
 setInterval(displayTime, 1000);
-console.log();
-function calendarColor() {}
-let now = new Date().getHours();
-if (now > 8) {
+
+let presentTime = new Date().getHours();
+if (presentTime > 9) {
   $("#time1").addClass("past");
-} else if (now >= 8 && now < 9) {
+} else if (presentTime >= 9 && presentTime < 10) {
   $("#time1").addClass("present");
-} else if (now < 8) {
+} else if (presentTime < 9) {
   $("#time1").addClass("future");
 }
 
-if (now > 9) {
+if (presentTime > 10) {
   $("#time2").addClass("past");
-} else if (now >= 9 && now < 10) {
+} else if (presentTime >= 10 && presentTime < 11) {
   $("#time2").addClass("present");
-} else if (now < 9) {
+} else if (presentTime < 10) {
   $("#time2").addClass("future");
 }
 
-if (now > 10) {
+if (presentTime > 11) {
   $("#time3").addClass("past");
-} else if (now >= 10 && now < 11) {
+} else if (presentTime >= 11 && presentTime < 12) {
   $("#time3").addClass("present");
-} else if (now < 10) {
+} else if (presentTime < 11) {
   $("#time3").addClass("future");
 }
 
-if (now > 11) {
+if (presentTime > 12) {
   $("#time4").addClass("past");
-} else if (now >= 11 && now < 12) {
+} else if (presentTime >= 12 && presentTime < 13) {
   $("#time4").addClass("present");
-} else if (now < 11) {
+} else if (presentTime < 12) {
   $("#time4").addClass("future");
 }
 
-if (now > 12) {
+if (presentTime > 13) {
   $("#time5").addClass("past");
-} else if (now >= 12 && now < 13) {
+} else if (presentTime >= 13 && presentTime < 14) {
   $("#time5").addClass("present");
-} else if (now < 12) {
+} else if (presentTime < 13) {
   $("#time5").addClass("future");
 }
 
-if (now > 13) {
+if (presentTime > 14) {
   $("#time6").addClass("past");
-} else if (now >= 13 && now < 14) {
+} else if (presentTime >= 14 && presentTime < 15) {
   $("#time6").addClass("present");
-} else if (now < 13) {
+} else if (presentTime < 14) {
   $("#time6").addClass("future");
 }
 
-if (now > 14) {
+if (presentTime > 15) {
   $("#time7").addClass("past");
-} else if (now >= 14 && now < 15) {
+} else if (presentTime >= 15 && presentTime < 16) {
   $("#time7").addClass("present");
-} else if (now < 14) {
+} else if (presentTime < 15) {
   $("#time7").addClass("future");
 }
 
-if (now > 15) {
+if (presentTime > 16) {
   $("#time8").addClass("past");
-} else if (now >= 15 && now < 16) {
+} else if (presentTime >= 16 && presentTime < 17) {
   $("#time8").addClass("present");
-} else if (now < 15) {
+} else if (presentTime < 16) {
   $("#time8").addClass("future");
 }
 
-if (now > 16) {
+if (presentTime > 17) {
   $("#time9").addClass("past");
-} else if (now >= 16 && now < 17) {
+} else if (presentTime >= 17 && presentTime < 18) {
   $("#time9").addClass("present");
-} else if (now < 16) {
+} else if (presentTime < 17) {
   $("#time9").addClass("future");
 }
+
+$(save1).on("click", function (event) {
+  event.preventDefault();
+  // let thingsToDO = $("#time1").val();
+  // localStorage.setItem("Things to be done", thingsToDO);
+  let ThingsToDO = localStorage.setItem("9am TTD", $("#time1").val());
+});
+$("#time1").val(localStorage.getItem("9am TTD"));
+
+$(save2).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("10amTTD", $("#time2").val());
+});
+$("#time2").val(localStorage.getItem("10amTTD"));
+
+$(save3).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("11amTTD", $("#time3").val());
+});
+$("#time3").val(localStorage.getItem("11amTTD"));
+
+$(save4).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("12pmTTD", $("#time4").val());
+});
+$("#time4").val(localStorage.getItem("12pmTTD"));
+
+$(save5).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("1pmTTD", $("#time5").val());
+});
+$("#time5").val(localStorage.getItem("1pmTTD"));
+
+$(save6).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("2pmTTD", $("#time6").val());
+});
+$("#time6").val(localStorage.getItem("2pmTTD"));
+
+$(save7).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("3pmTTD", $("#time7").val());
+});
+$("#time7").val(localStorage.getItem("3pmTTD"));
+
+$(save8).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("4pmTTD", $("#time8").val());
+});
+$("#time8").val(localStorage.getItem("4pmTTD"));
+
+$(save9).on("click", function (event) {
+  event.preventDefault();
+
+  let ThingsToDO = localStorage.setItem("5pmTTD", $("#time9").val());
+});
+$("#time9").val(localStorage.getItem("5pmTTD"));
