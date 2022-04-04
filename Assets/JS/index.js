e8b41f9b843899a5310c53e22e7d520d422e0969
Bryson Kern <kernbryson@yahoo.com>
2022-04-04 15:54:03 -0400

    finished ReadME, Fixed links to JS and CSS. Shrunk javascript code and added comments

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,154 +1,86 @@
 // save reference to important DOM elements
 var timeDisplayEl = $("#time-display");
 // handle displaying the time
-let save1 = $("#save1");
-let save2 = $("#save2");
-let save3 = $("#save3");
-let save4 = $("#save4");
-let save5 = $("#save5");
-let save6 = $("#save6");
-let save7 = $("#save7");
-let save8 = $("#save8");
-let save9 = $("#save9");
+//displayTime creates the clock
 function displayTime() {
   var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
 setInterval(displayTime, 1000);
+// function decides when to use correct time classes
+let presentTime = moment().hours();
 
-let presentTime = new Date().getHours();
-if (presentTime > 9) {
-  $("#time1").addClass("past");
-} else if (presentTime >= 9 && presentTime < 10) {
-  $("#time1").addClass("present");
-} else if (presentTime < 9) {
-  $("#time1").addClass("future");
+for (let i = 0; i < 9; i++) {
+  let timeID = i + 9;
+  let timeBlock = $("#time" + timeID);
+  if (timeID < presentTime) {
+    timeBlock.addClass("past");
+  } else if (timeID === presentTime) {
+    timeBlock.addClass("present");
+  } else {
+    timeBlock.addClass("future");
+  }
 }
-
-if (presentTime > 10) {
-  $("#time2").addClass("past");
-} else if (presentTime >= 10 && presentTime < 11) {
-  $("#time2").addClass("present");
-} else if (presentTime < 10) {
-  $("#time2").addClass("future");
-}
-
-if (presentTime > 11) {
-  $("#time3").addClass("past");
-} else if (presentTime >= 11 && presentTime < 12) {
-  $("#time3").addClass("present");
-} else if (presentTime < 11) {
-  $("#time3").addClass("future");
-}
-
-if (presentTime > 12) {
-  $("#time4").addClass("past");
-} else if (presentTime >= 12 && presentTime < 13) {
-  $("#time4").addClass("present");
-} else if (presentTime < 12) {
-  $("#time4").addClass("future");
-}
-
-if (presentTime > 13) {
-  $("#time5").addClass("past");
-} else if (presentTime >= 13 && presentTime < 14) {
-  $("#time5").addClass("present");
-} else if (presentTime < 13) {
-  $("#time5").addClass("future");
-}
-
-if (presentTime > 14) {
-  $("#time6").addClass("past");
-} else if (presentTime >= 14 && presentTime < 15) {
-  $("#time6").addClass("present");
-} else if (presentTime < 14) {
-  $("#time6").addClass("future");
-}
-
-if (presentTime > 15) {
-  $("#time7").addClass("past");
-} else if (presentTime >= 15 && presentTime < 16) {
-  $("#time7").addClass("present");
-} else if (presentTime < 15) {
-  $("#time7").addClass("future");
-}
-
-if (presentTime > 16) {
-  $("#time8").addClass("past");
-} else if (presentTime >= 16 && presentTime < 17) {
-  $("#time8").addClass("present");
-} else if (presentTime < 16) {
-  $("#time8").addClass("future");
-}
-
-if (presentTime > 17) {
-  $("#time9").addClass("past");
-} else if (presentTime >= 17 && presentTime < 18) {
-  $("#time9").addClass("present");
-} else if (presentTime < 17) {
-  $("#time9").addClass("future");
-}
-
+// This section saves the input to local storage
 $(save1).on("click", function (event) {
   event.preventDefault();
-  // let thingsToDO = $("#time1").val();
-  // localStorage.setItem("Things to be done", thingsToDO);
-  let ThingsToDO = localStorage.setItem("9am TTD", $("#time1").val());
+
+  let ThingsToDO = localStorage.setItem("9am TTD", $("#time9").val());
 });
-$("#time1").val(localStorage.getItem("9am TTD"));
+$("#time9").val(localStorage.getItem("9am TTD"));
 
 $(save2).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("10amTTD", $("#time2").val());
+  let ThingsToDO = localStorage.setItem("10amTTD", $("#time10").val());
 });
-$("#time2").val(localStorage.getItem("10amTTD"));
+$("#time10").val(localStorage.getItem("10amTTD"));
 
 $(save3).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("11amTTD", $("#time3").val());
+  let ThingsToDO = localStorage.setItem("11amTTD", $("#time11").val());
 });
-$("#time3").val(localStorage.getItem("11amTTD"));
+$("#time11").val(localStorage.getItem("11amTTD"));
 
 $(save4).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("12pmTTD", $("#time4").val());
+  let ThingsToDO = localStorage.setItem("12pmTTD", $("#time12").val());
 });
-$("#time4").val(localStorage.getItem("12pmTTD"));
+$("#time12").val(localStorage.getItem("12pmTTD"));
 
 $(save5).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("1pmTTD", $("#time5").val());
+  let ThingsToDO = localStorage.setItem("1pmTTD", $("#time13").val());
 });
-$("#time5").val(localStorage.getItem("1pmTTD"));
+$("#time13").val(localStorage.getItem("1pmTTD"));
 
 $(save6).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("2pmTTD", $("#time6").val());
+  let ThingsToDO = localStorage.setItem("2pmTTD", $("#time14").val());
 });
-$("#time6").val(localStorage.getItem("2pmTTD"));
+$("#time14").val(localStorage.getItem("2pmTTD"));
 
 $(save7).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("3pmTTD", $("#time7").val());
+  let ThingsToDO = localStorage.setItem("3pmTTD", $("#time15").val());
 });
-$("#time7").val(localStorage.getItem("3pmTTD"));
+$("#time15").val(localStorage.getItem("3pmTTD"));
 
 $(save8).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("4pmTTD", $("#time8").val());
+  let ThingsToDO = localStorage.setItem("4pmTTD", $("#time16").val());
 });
-$("#time8").val(localStorage.getItem("4pmTTD"));
+$("#time16").val(localStorage.getItem("4pmTTD"));
 
 $(save9).on("click", function (event) {
   event.preventDefault();
 
-  let ThingsToDO = localStorage.setItem("5pmTTD", $("#time9").val());
+  let ThingsToDO = localStorage.setItem("5pmTTD", $("#time17").val());
 });
-$("#time9").val(localStorage.getItem("5pmTTD"));
+$("#time17").val(localStorage.getItem("5pmTTD"));
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# HW5WorkDayScheduler
+# Word Day Scheduler
+
+https://kernbryson.github.io/HW5WorkDayScheduler/ [![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](https://kernbryson.github.io/HW5WorkDayScheduler//)
+
+## Description
+
+For this project I created a work day scheduler. This scheduler allows users to keep up with what they need to do throughout the work day. My motivation behind this project was to create a working scheduler to improve my javascript skills. This scheduler makes it easier for users to get things done during their day because everything they need to do is all in one central location. While creating this project I learned a lot about local storage and creating elements in javascript.
+
+## Table of Contents
+
+- [Creation](#Creation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Creation
+
+In order to create this scheduler, I first created my HTML. I used a lot of components from bootstrap to make my project look professional while also saving time. I then began creating ID's and styling my HTML with my style page. The first main task I wanted to complete was to create a clock through Moment JS. In order to properly display the time I used the "moment().format" statement. After I got that out of the way I began working on the save buttons. I first turned all of the right hand columns I got from bootstrap into buttons so I could then later give them a function in my javascript. I also used Font Awesome to obtain custom save icons for the buttons. After I created my save buttons I began to work on my javascript. The first hurdle I had to jump over was creating a function that would dynamically change the color of each time block depending on the time. In order to do this, I created a function with a for loop that added the class of past, present, or future depending on the current time. The last difficult task was saving all the user's data to local storage. In order to save all the tasks that have been submitted I made multiple functions that get the users text input once they clicked the save button. At the end of each of those functions I called the data back with the "get item" command.
+
+![Code Usage](./Assets/Photos/readme.png)
+
+## Usage
+
+The scheduler is very easy to use. All the user needs to do is type in the task or reminder they want that corresponds with the correct time block and click the save button. The user can then leave the page and come back whenever they need a refresher and their previous data will still be displayed. The coloration with each time block helps users keep up with the current time and what tasks they should be working on.
+
+## Credits
+
+- Professional README - https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+- Web Dev Simplified - https://www.youtube.com/c/WebDevSimplified
+- Bootstrap - https://getbootstrap.com/
+- Moment js - https://momentjs.com/
+
+## License
+
+Copyright (c) [2022] [Bryson Kern]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
